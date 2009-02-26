f5efd633558314dd50abc8967033e7447a8360e6
vigliag <vigliag@a8ebc27c-002e-11de-8c4e-4bbb75a408b7>
2009-02-26 17:39:14 +0000

    Ok, è tornato a funzionare, ho cambiato la struttura delle cartelle e, purtroppo, disattivato qualche funzione. Almeno però ora va :) Ah, ho anche incluso spyc

---
+++ trunk/managegrid.php
@@ -0,0 +1,24 @@
+<?php
+include "lib/libcore.php"; /*($_SERVER['DOCUMENT_ROOT']."lib/libcore.php"); Non ne vedo l'utilità :P */
+###################################
+# MyMosaic Managegrid             #
+###################################
+
+function salvagriglia($dati,$filegriglia="griglia.txt"){
+    if (write_file($filegriglia,$dati)){
+        echo "Modifiche alla griglia salvate correttamente";
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+};
+
+function caricagriglia($filegriglia="griglia.txt"){
+    $griglia = get_file($filegriglia);
+    return $griglia;
+};
+
+if ($_POST["dati"]){
+    salvagriglia($_POST["dati"]);
+};
+?>
